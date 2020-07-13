d7e88ac5b98425b6fe11aa86483118da8c5f9b9d
glitch <saniltison@gmail.com>
2020-07-13 12:19:31 -0400

    found the cause of the problem from the last commit
    - in the "create_table.py" file, the word "INTEGER" was spelt "INTERGER"
    
    - once that was corrected the problem was fixed
    
    - code works fine at the end of video # 81 & #82

--- code/__pycache__/create_tables.py
+++ code/__pycache__/create_tables.py
@@ -4,7 +4,7 @@ connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
 
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTERGER PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 
 cursor.execute(create_table)
 
