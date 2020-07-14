a8600df11dd5dce5d5c8b03158c52915b41c0f7d
glitch <saniltison@gmail.com>
2020-07-14 17:57:21 -0400

    this commit was done at the end of vido #86 'putting error handling in code with 'try & execpt' construct'
    - code was not tested because it was not tested by instructor

--- code1/item.py
+++ code1/item.py
@@ -16,7 +16,11 @@ class Item(Resource):
 
     # @jwt_required()
     def get(self, name):
-        item = self.find_by_name(name)
+        try:
+            item = self.find_by_name(name)
+        except:
+            return {'message':"there was a problem with SQL while retrieving the item."}, 500
+        
         if item:
             return item
         return {'message':"Item not found"}, 404 
@@ -39,9 +43,30 @@ class Item(Resource):
             return {'message':"An item already exist with '{}'".format(name),},400
        
         data = Item.parser.parse_args()
-       
         item = {'name': name, 'price': data['price']}
+        
 
+        try:
+            self.insert(item)
+        except:
+            return {'message':"an error occurred when inserting that new item"}, 500
+        
+        """
+        - this 'try & except' python construct will do the following
+            - it will try to execute the code within the 'try' block
+            - if that code does not return any error then it does not
+              go into the 'execpt' portion of the code
+            - if there was an error in executing the code in the
+              'try' block, then python will return the message within
+              the 'except' block of code
+            - that message should be short but detailed enough to
+              tell the programmer where in the body of the code the
+              error happened
+        """
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -50,8 +75,20 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
+        """
+        - this 'classmethod' will be used by the 'post' and 'put'
+        method to insert items or update items in the database
+        
+        - the code could have stayed in the 'post' method but when
+          the 'put' method calls up the 'post' method to insert an
+          item into the database, there will be code that will be
+          executed that is not needed to be executed by the 'put'
+          method
 
-        return item, 201
+        - instead of taking the name of a item as a variable, this
+          method will take an item with its properties
+        
+        """
        
     def delete(self, name):
         if self.find_by_name(name):    
